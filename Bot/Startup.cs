d22532ba3047de6a27eab5fbe939aeea598e3df7
analopi <vit.k2000@yandex.ru>
2021-12-11 17:45:35 +0500

    test2

--- Bot/Startup.cs
+++ Bot/Startup.cs
@@ -9,7 +9,7 @@ namespace Bot
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            string con = "Server=(localdb)\\mssqllocaldb;Database=BOTOK;Trusted_Connection=True;";
+            string con = "postgres://mdsbrugikhkypc:47722fb09dd22681a70e992db13c58a5dce1eba18ae15a7fa585df05c87cf7b9@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/d8res2n8jior24";
             // устанавливаем контекст данных
             services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
  
