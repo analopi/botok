a1bc0587f5e12e9905821b212e87bb9cc67fa563
analopi <vit.k2000@yandex.ru>
2021-12-11 18:59:39 +0500

    test6

---
+++ Bot/Controllers/CallbackController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CallbackController : Controller
+    {
+        // GET: CallbackController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CallbackController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CallbackController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CallbackController/Create
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            return Content("ae02c630");
+        }
+
+    }
+}
