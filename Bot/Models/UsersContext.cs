651aa0df467e7fdcef6b3736042f51f4bd708091
analopi <vit.k2000@yandex.ru>
2021-12-11 16:42:42 +0500

    init

---
+++ Bot/Models/UsersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bot.Models
+{
+    public class UsersContext : DbContext
+    {
+         public DbSet<User> Users { get; set; }
+         public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+         {
+             Database.EnsureCreated();
+         }
+    }
+}
+
