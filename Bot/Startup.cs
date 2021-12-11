4571d89095034b233e79417c4e841e307b3b9adc
analopi <vit.k2000@yandex.ru>
2021-12-11 17:57:01 +0500

    test3

--- Bot/Startup.cs
+++ Bot/Startup.cs
@@ -9,7 +9,7 @@ namespace Bot
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            string con = "postgres://mdsbrugikhkypc:47722fb09dd22681a70e992db13c58a5dce1eba18ae15a7fa585df05c87cf7b9@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/d8res2n8jior24";
+            string con = "Server=ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432; Database = d8res2n8jior24; User ID = mdsbrugikhkypc; PASSWORD = 47722fb09dd22681a70e992db13c58a5dce1eba18ae15a7fa585df05c87cf7b9; Trusted_Connection = True";
             // устанавливаем контекст данных
             services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
  
