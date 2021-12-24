dc9514b3a7b74415095807779f58ad294ded6b58
analopi <vit.k2000@yandex.ru>
2021-12-24 14:29:33 +0500

    rrr

--- Bot/Controllers/CallbackController.cs
+++ Bot/Controllers/CallbackController.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 
 namespace Bot.Controllers
 {
@@ -19,7 +21,7 @@ namespace Bot.Controllers
     {
         private readonly IConfiguration _configuration;
 
-        
+
         public CallbackController(IConfiguration configuration)
         {
             _configuration = configuration;
@@ -29,24 +31,42 @@ namespace Bot.Controllers
         [HttpPost]
         public IActionResult Callback([FromBody] Updates updates)
         {
+
+            var cl = new WebClient { Encoding = Encoding.UTF8 };
             // Проверяем, что находится в поле "type"
             switch (updates.Type)
             {
-                
+
                 // Если это уведомление для подтверждения адреса
                 case "confirmation":
                     // Отправляем строку для подтверждения 
                     return Ok("ae02c630");
                 case "message_new":
-
+                    var ID = updates.Object["message"]["from_id"].ToString();
+                   
+                    var ms = $"Ваш Id равен {ID}";
                     //return Json(updates.Object);
-                    return Content($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id=70259283&message=тыеблан&random_id=0");
+                    //return Content($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id=70259283&message=тыеблан&random_id=1");
+                    cl.DownloadString($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id={ID}&message={ms}&random_id=1");
 
+                    //this.poshel();
+                    return Ok("ok");
 
             }
             // Возвращаем "ok" серверу Callback API
             return Ok("ok");
 
         }
+       // public async void poshel()
+       // {
+          
+            //var client = new HttpClient();
+            
+           // string l = @"[{'key':'random_id','value':'0','type':'text','enabled':true},{'key':'peer_id','value':'70259283','type':'text','enabled':true},{'key':'user_id','value':'70259283','type':'text','enabled':true},{'key':'message','value':'ты еблан','type':'text','enabled':true}]";
+            //var p = await client.PostAsync($"https://api.vk.com/method/messages.send?access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca", new StringContent(l, Encoding.UTF8, "application/x-form-urlencoded"));
+            //var c = await client.PostAsJsonAsync("", @"{ peer_id = 70259283, user_id = 70259283,random_id = 1,message = 'тыеблан', v = 5.131}");
+            //var c = await c.Content.ReadAsAsync<Post>;
+        //}
+
     }
 }
