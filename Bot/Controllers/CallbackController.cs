1c29ccb85e2d89621713950af8b81c6ce8c6e536
analopi <vit.k2000@yandex.ru>
2022-01-03 16:42:53 +0500

    rock

--- Bot/Controllers/CallbackController.cs
+++ Bot/Controllers/CallbackController.cs
@@ -32,7 +32,7 @@ namespace Bot.Controllers
         public IActionResult Callback([FromBody] Updates updates)
         {
 
-            var cl = new WebClient { Encoding = Encoding.UTF8 };
+            //var cl = new WebClient { Encoding = Encoding.UTF8 };
             // Проверяем, что находится в поле "type"
             switch (updates.Type)
             {
@@ -42,14 +42,14 @@ namespace Bot.Controllers
                     // Отправляем строку для подтверждения 
                     return Ok("ae02c630");
                 case "message_new":
-                    var ID = updates.Object["message"]["from_id"].ToString();
-                    var rand = updates.Object["message"]["random_id"].ToString();
-                    var ms = $"Ваш Id равен {ID}";
+                    //var ID = updates.Object["message"]["from_id"].ToString();
+                    //var rand = updates.Object["message"]["random_id"].ToString();
+                    //var ms = $"Ваш Id равен {ID}";
                     //return Json(updates.Object);
                     //return Content($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id=70259283&message=тыеблан&random_id=1");
-                    cl.DownloadString($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id={ID}&message={ms}&random_id={rand}");
+                    //cl.DownloadString($"https://api.vk.com/method/messages.send?v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&user_id={ID}&message={ms}&random_id={rand}");
 
-                    //this.poshel();
+                    this.poshel();
                     return Ok("ok");
 
             }
@@ -57,16 +57,15 @@ namespace Bot.Controllers
             return Ok("ok");
 
         }
-       // public async void poshel()
-       // {
+        public async void poshel()
+        {
           
-            //var client = new HttpClient();
-            
-           // string l = @"[{'key':'random_id','value':'0','type':'text','enabled':true},{'key':'peer_id','value':'70259283','type':'text','enabled':true},{'key':'user_id','value':'70259283','type':'text','enabled':true},{'key':'message','value':'ты еблан','type':'text','enabled':true}]";
-            //var p = await client.PostAsync($"https://api.vk.com/method/messages.send?access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca", new StringContent(l, Encoding.UTF8, "application/x-form-urlencoded"));
-            //var c = await client.PostAsJsonAsync("", @"{ peer_id = 70259283, user_id = 70259283,random_id = 1,message = 'тыеблан', v = 5.131}");
-            //var c = await c.Content.ReadAsAsync<Post>;
-        //}
+            var client = new HttpClient();
+           
+
+            string l = "v=5.131&access_token=e954287faaa675dc6b387fe3ad1459ad89fba3235eaf0d02a4cd7dc2bdccd51c8881c0b23663593f22aca&random_id=0&user_id=70259283&message=123";
+            var p = await client.PostAsync($"https://api.vk.com/method/messages.send?", new StringContent(l, Encoding.UTF8, "application/x-www-form-urlencoded"));
+        }
 
     }
 }
---
+++ Bot/IWebInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot
+{
+    /// <summary>
+    /// класс реализуемого интерфейса должен быть статическим
+    /// </summary>
+    interface IWebInterface
+    {
+        /// <summary>
+        /// отправляет стандартное отображение кнопок и их значения
+        /// принимает id пользователя, список списков кортежей и сообщение
+        /// внутренний список - кнопки в ряд
+        /// внешний список - ряды кнопок 
+        /// или по нормальному список рядов кнопок
+        /// в кортеже(кнопке)) первое значение - текст кнопки(label), второе значение - то что кнопка возрващает при нажатии(payload)
+        /// https://dev.vk.com/api/bots/development/keyboard
+        /// </summary>
+        /// <param name="buttons"></param>
+        void SendButtons(long id, string message, List<List<(string, string)>> buttons);
+
+        /// <summary>
+        /// отправляет сообщение
+        /// принимает id пользователя и сообщение
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="message"></param>
+        void SendMessage(long id, string message);
+    }
+}
